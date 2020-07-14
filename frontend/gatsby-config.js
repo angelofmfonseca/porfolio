6605940bc3624bf57fc27dec40f03e54ca125e6d
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-07-14 17:18:01 -0300

    blog title

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Portfolio`,
-    description: `A Front End Developer Portfolio`,
+    title: `Blog`,
+    description: `A Front End Developer Blog`,
     author: `@angelofonseca`,
   },
   plugins: [
