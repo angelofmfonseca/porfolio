9edd1aec6e2233fcba2f0e7e4edc115cf14728fd
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-07-14 16:48:08 -0300

    gatsby-link between home and about

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from "gatsby"
 
 const AboutPage = () => {
   return (
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <ul>
+        <li><Link to="/" activeStyle={{ color: "red" }}>Home</Link></li>
+        <li><Link to="/about" activeStyle={{ color: "red" }}>About</Link></li>
+      </ul>
+    </>
   )
 }
 
