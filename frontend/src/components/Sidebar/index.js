13fb1a4d541d534f443a31419a4ddd0f1212b7bd
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-07-15 19:49:39 -0300

    Sidebar component + styles

---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import * as S from "./styles"
+import Profile from "../Profile"
+
+const Sidebar = () => (
+    <S.SideBarWrapper>
+      <Profile />
+    </S.SideBarWrapper>
+  )
+
+export default Sidebar
---
+++ frontend/src/components/Sidebar/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const SideBarWrapper = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  position: fixed;
+  width: 20rem;
+  height: 100vh;
+  padding: 2rem;
+  background: #192734;
+  border-right: 1px solid #38444d;
+`;
