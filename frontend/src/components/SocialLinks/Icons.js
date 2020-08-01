c23492fc8f4c20d4bd736d3cf3178fd29e477c7f
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-08-01 17:49:24 -0300

    Github icons changed to Icomoon

--- frontend/src/components/SocialLinks/Icons.js
+++ frontend/src/components/SocialLinks/Icons.js
@@ -1,4 +1,4 @@
-import { Github } from "@styled-icons/fa-brands/Github"
+import { Github } from "@styled-icons/icomoon/Github"
 import { Linkedin } from "@styled-icons/fa-brands/Linkedin"
 
 const Icons = {
