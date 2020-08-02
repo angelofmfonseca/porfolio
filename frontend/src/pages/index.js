451c315751718cac9513740ea89d3b77ee884e1f
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-08-02 14:35:32 -0300

    posts dinâmicos com Markdown e GraphQL

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,22 +1,62 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import PostItem from "../components/PostItem"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem
-      slug="about"
-      background="#FF0000"
-      color="#fff"
-      category="life"
-      date="21 de jilho de 2017"
-      title="Virada de chave"
-      description="Os desafios de uma carreira drástica de carreira próximo dos 30 anos"
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query PostList {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-BR")
+                category
+                title
+                description
+                background
+                color
+              }
+            }
+          }
+        }
+      }
+    `
+  )
 
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: {
+              date,
+              title,
+              description,
+              category,
+              background,
+              color,
+            },
+          },
+        }) => (
+          <PostItem
+            slug="/about/"
+            background={background}
+            color={color}
+            category={category}
+            date={date}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
 export default IndexPage
